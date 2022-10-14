b5ddd31817deeba5e8df8e3a2f47e118251f68b5
evols101 <asimiyumusa@gmail.com>
2022-10-14 08:01:55 -0700

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <string.h>
+
+/**
+
+ * * main - Entry point
+
+ *
+
+ * * Description: 'prints to the standard output using printf function'
+
+ *
+
+ * * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+  char string[] = "with proper grammar, but the outcome is a piece of art,";
+  
+  printf("%s\n", string);
+  
+  return (0);
+  
+}
