1a66c88b700b47bcfa2d8a0423aff308ac9432fb
evols101 <asimiyumusa@gmail.com>
2022-10-14 21:56:50 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
-
 #include <string.h>
-
 /**
-
  * * main - Entry point
-
  *
-
  * * Description: 'prints to the standard output using puts function'
-
  *
-
  * * Return: Always 0 (Success)
-
  */
-
 int main(void)
-  
 {
-  
   char string[] = "\"Programming is like building a multilingual puzzle";
-  
   puts(string);
-  
   return (0);
-  
 }
