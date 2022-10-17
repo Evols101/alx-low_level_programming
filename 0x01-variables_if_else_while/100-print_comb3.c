f36694edd006180b44b3da32c0e9be3784b9bcb6
evols101 <asimiyumusa@gmail.com>
2022-10-17 13:43:47 -0700

    variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'prints only using the putchar'
+*
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+int n = 1;
+int m;
+int col;
+int row = 0;
+for (col = 0; col < 100; col += 10)
+{
+row++;
+for (m = 1; m < 11; m++)
+{
+if (n < (col + row) || n % 10 == 0)
+{
+}
+else
+{
+putchar(n / 10 + '0');
+putchar(n % 10 + '0');
+if (n < 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+n++;
+}
+}
+putchar('\n');
+return (0);
+}
